f37ff5a9a62810bcd728436473a7eca8237e37f2
saurav-ghimire <sauravghimire0123@gmail.com>
2024-06-15 11:24:29 -0400

    Context api integrated and add to cart and remove from cart feature integrated

--- app/components/FoodItemCard/FoodItemCard.jsx
+++ app/components/FoodItemCard/FoodItemCard.jsx
@@ -1,31 +1,29 @@
 import Image from 'next/image';
 import './FoodItemCard.css';
 import { assets } from '@/app/assets/assets';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { FaPlus, FaMinus } from "react-icons/fa";
-
-
+import { storeContext } from '@/app/context/storeContext';
 
 function FoodItemCard({ item }) {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = storeContext();
+
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         <Image src={item?.image} layout="fill" className="food-item-image" alt={item?.name} />
-        <Image src={assets.add_icon_white} alt="" />
         {
-          !itemCount 
-          ? <FaPlus className="first-add" onClick={() => setItemCount(prev=>prev+1)}  /> 
-          
-          :   <div className="food-item-counter">
-          <div className="icon-container" onClick={() => setItemCount(prev=>prev+1)}>
-            <FaPlus className="plus-icon" />
-          </div>
-          <span className="item-count">{itemCount}</span>
-          <div className="icon-container" onClick={() => setItemCount(prev=>prev-1)}>
-            <FaMinus className="minus-icon" />
-          </div>
-        </div>
+          !cartItems[item?._id] 
+            ? <FaPlus className="first-add" onClick={() => addToCart(item._id)} /> 
+            : <div className="food-item-counter">
+                <div className="icon-container" onClick={() => addToCart(item._id)}>
+                  <FaPlus className="plus-icon" />
+                </div>
+                <span className="item-count">{cartItems[item._id]}</span>
+                <div className="icon-container" onClick={() => removeFromCart(item._id)}>
+                  <FaMinus className="minus-icon" />
+                </div>
+              </div>
         }
       </div>
       <div className="food-item-info">
@@ -35,11 +33,10 @@ function FoodItemCard({ item }) {
         <p className="food-item-desc">{item?.description}</p>
         <p className="food-item-price">
           <span>
-            ${item?.price} 
-
-              {item?.discount && (
-                <span className="food-item-discount">-{item?.discount}%</span>
-              )}
+            ${item?.price}
+            {item?.discount && (
+              <span className="food-item-discount">-{item?.discount}%</span>
+            )}
           </span>
           <Image src={assets.rating_starts} alt="rating" />
         </p>
--- app/context/storeContext.jsx
+++ app/context/storeContext.jsx
@@ -1,13 +1,45 @@
 "use client"
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { food_list } from '../assets/assets';
 
 const MyContext = createContext(null);
 
 export const MyProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => {
+      if (!prev[itemId]) {
+        return { ...prev, [itemId]: 1 };
+      } else {
+        return { ...prev, [itemId]: prev[itemId] + 1 };
+      }
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      if (prev[itemId] > 1) {
+        return { ...prev, [itemId]: prev[itemId] - 1 };
+      } else {
+        const { [itemId]: _, ...rest } = prev;
+        return rest;
+      }
+    });
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
   const contextValue = {
-    food_list
-  }
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
+  };
+
   return (
     <MyContext.Provider value={contextValue}>
       {children}
